d42cf35169171680741604b6d6e76395236e24cf
sumanwmit <suman.wmit@gmail.com>
2026-08-13 09:27:37 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import { BUSINESS_INFO } from '../data/websiteData';
 export default function Footer() {
   // Global Tracking Hook as instructed
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
